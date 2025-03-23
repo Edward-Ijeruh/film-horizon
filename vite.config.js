7534b28437d92f92247e107845449b9212d46d49
Edward Ijeruh <zacharaiah17@gmail.com>
2025-03-23 15:20:44 +0100

    Update project with new features/bug fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'film-horizon',
+  base: './',
+  base: '/film-horizon/',
   plugins: [
     react(),
     tailwindcss(),
